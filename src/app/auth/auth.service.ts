b25ace45b760a25bdd75d0df3bc9ad9b645794f3
SonAkira666 <Son_Akira666@hotmail.com>
2020-12-16 17:32:54 +0100

    Semplificato il messaggio di errore autenticazione

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -15,6 +15,12 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+  static errorMessages = {
+    'EMAIL_EXISTS': 'This email exists already!',
+    'EMAIL_NOT_FOUND': 'Non c\'è sto tizio',
+    'INVALID_PASSWORD': 'Password sbagliatissima!'
+  }
+
   constructor(private http: HttpClient) {}
   signUp(email: string, password: string) {
     return this.postRequest('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC2V1OMIXUbpUC8Hu5jbB1E-VTN1P6RQi4', email, password);
@@ -29,18 +35,8 @@ export class AuthService {
       returnSecureToken: true
     }).pipe(catchError(errorResponse => {
       let errorMessage = 'An unknown error occured!';
-      if (errorResponse.error && errorResponse.error.error) {
-        switch (errorResponse.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email exists already!';
-            break;
-          case 'EMAIL_NOT_FOUND':
-            errorMessage = 'Non c\'è sto tizio';
-            break;
-          case 'INVALID_PASSWORD':
-            errorMessage = 'Password sbagliatissima!';
-            break;
-        }
+      if (errorResponse.error && errorResponse.error.error && AuthService.errorMessages[errorResponse.error.error.message]) {
+        errorMessage = AuthService.errorMessages[errorResponse.error.error.message];
       }
       return throwError(errorMessage);
     }));
